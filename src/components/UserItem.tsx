72b2ebeadecd80c30fda612bc02a57f887562a1f
Vitaliy <270vitalik270@gmail.com>
2021-11-16 13:44:51 +0200

    added base structure of project

---
+++ src/components/UserItem.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+import {IUser} from "../types/types";
+
+interface UserItemProps {
+  user: IUser
+}
+
+const UserItem: FC<UserItemProps> = ({ user }) => {
+  return (
+    <div style={{ padding: 15, border: '1px solid gray' }}>
+      {user.id}. {user.name} live in {user.address.city} on street {user.address.street}
+    </div>
+  );
+};
+
+export default UserItem;
